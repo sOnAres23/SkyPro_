d424f3a95d38855ed160c59063e651ce974fe188
Sergey Spisak <serega232003@gmail.com>
2024-06-13 12:47:48 +0300

    fixes

--- dop/num_1.py
+++ dop/num_1.py
@@ -4,12 +4,12 @@ def strings(words: list) -> list:
     new_list = []
     if words:
         for word in words:
-            if word == '' or word[0] == word[-1]:
+            if word == "" or word[0] == word[-1]:
                 new_list.append(word)
     return new_list
 
 
 if __name__ == "__main__":
-    print(strings(['hello', 'dear', 'Vladislav', 'you', 'are', 'talent']))
-    print(strings(['', 'madam', 'racecar', 'noon', 'level', '']))
+    print(strings(["hello", "dear", "Vladislav", "you", "are", "talent"]))
+    print(strings(["", "madam", "racecar", "noon", "level", ""]))
     print(strings([]))
